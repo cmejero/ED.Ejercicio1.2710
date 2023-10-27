61a96019cc496e12255d9295b5b6b205161cb515
csi23-cmejero <csi23-cmejero@colegioaltair.net>
2023-10-27 10:51:07 +0200

    no message

---
+++ edu.ED.Ejercicio1.2710/src/main/java/Inicio/Inicio.java
@@ -0,0 +1,28 @@
+package Inicio;
+
+import java.util.Scanner;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double num;
+		Scanner sc = new Scanner(System.in);
+		
+	    System.out.println("Pedir número por consola");
+	    num = sc.nextDouble();
+		
+        int i = (int) num;
+		String texto = String.valueOf(i);
+	
+		System.out.println(texto);
+		
+		
+		
+		
+		
+
+	}
+
+}
